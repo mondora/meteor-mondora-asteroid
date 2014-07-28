f0ef4ab48ac3033d69505a5b5bc019c86588a163
Paolo Scanferla <paolo.scanferla@mondora.com>
2014-07-28 12:12:49 +0200

    Moved files into top dir.

--- package.js
+++ package.js
@@ -4,6 +4,6 @@ Package.describe({
 
 Package.on_use(function (api) {
 	api.use(["webapp", "logging", "oauth"], "server");
-	api.add_files("lib/asteroid.js", "server");
-	api.add_files("lib/asteroid_oauth_popup.html", "server", {isAsset: true});
+	api.add_files("asteroid.js", "server");
+	api.add_files("asteroid_oauth_popup.html", "server", {isAsset: true});
 });
